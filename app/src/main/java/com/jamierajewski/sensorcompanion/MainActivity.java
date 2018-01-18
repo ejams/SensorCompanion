379b20e3c78e1e64d835fe58162dce922fb343ae
ejams <ejams.1thebest@gmail.com>
2018-01-17 21:23:00 -0700

    Calibration screen basic layout complete

--- app/src/main/java/com/jamierajewski/sensorcompanion/MainActivity.java
+++ app/src/main/java/com/jamierajewski/sensorcompanion/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    //public static final String EXTRA_MESSAGE = "com.jamierajewski.sensorcompanion.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void newMeasurement(View view){
-
+        Intent intent = new Intent(this, CalibrationActivity.class);
+        startActivity(intent);
     }
 }
